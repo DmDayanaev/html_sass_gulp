a33884afe84ba0cb4b226c7330789c8ccafeeb16
DmDayanaev <dda2004@bk.ru>
2018-02-16 17:25:10 +0300

    добавил проверку пароля

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,26 @@ if (age >= 18) {
 			alert ("Тебе еще рано.");
 			window.location.href = "https://google.com";
 		}
+
+
+
+while (login !== "guest") {
+	var login = prompt("Введите логин");
+	if (login == "guest") {
+		alert ("Добро пожаловать гость");
+	} else if (login == "admin"||login == "user") {
+		var pas = prompt("Введите пароль");
+		switch(pas) {
+			case "123":
+			case "777":
+			case "333":	alert ("Добро пожаловать"); 
+			
+			login = "guest"; // вот здесь не придумал как выйте из цикла - только так, может еще как-то можно?
+			
+			break;
+			default: alert ("Пароль не верный");
+		}
+	} else {
+		alert ("Пользователя " +login+ " нет в системе");
+	}	
+}
